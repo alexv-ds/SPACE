a97a341ab4fdf2d4c775d31d0d5604186a0637cb
sasichkamega <sasichkamega@gmail.com>
2023-01-06 16:02:32 +0300

    update transform

--- engine/modules/transform.cpp
+++ engine/modules/transform.cpp
@@ -1,9 +1,27 @@
 #include "transform.hpp"
 
+namespace engine::transform::detail {
+
+} //namespace engine::transform::detail
+
 namespace engine {
+using namespace transform;
 
 Transform::Transform(flecs::world& world) {
   world.module<Transform>("transform");
+  
+  world.component<Position2>()
+    .member<decltype(Position2::x)>("x")
+    .member<decltype(Position2::y)>("y");
+
+  world.component<Scale2>()
+    .member<decltype(Scale2::x)>("x")
+    .member<decltype(Scale2::y)>("y");
+
+  world.component<Rotation2>()
+    .member<decltype(Rotation2::angle), flecs::units::angle::Radians>("angle");
+
+  world.component<Transform2>();
 }
 
 } //namespace engine
--- engine/modules/transform.hpp
+++ engine/modules/transform.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include <flecs.h>
+#include <glm/mat3x3.hpp>
 
 namespace engine {
 
@@ -11,7 +12,22 @@ struct Transform {
 
 namespace engine::transform {
 
+struct Position2 {
+  float x;
+  float y;
+};
 
+struct Scale2 {
+  float x;
+  float y;
+};
 
+struct Rotation2 {
+  float angle; //in radians
+};
+
+struct Transform2 {
+  glm::mat3 value;
+};
 
 } //namespace engine::transform
