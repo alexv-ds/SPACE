6e06c3c9bf96334fdaab8dcc77e4932101a0a2c5
alex <sasichkamega@gmail.com>
2023-01-08 22:38:29 +0300

    shader stuff

--- engine/main.cpp
+++ engine/main.cpp
@@ -6,11 +6,13 @@
 #include "modules/window-backend-sfml.hpp"
 #include "modules/filesystem.hpp"
 #include "modules/filesystem-backend-stdlib.hpp"
+#include "modules/transform.hpp"
+#include "modules/opengl.hpp"
 
 int main(int argc, char const *argv[]) {
   engine::setup_spdlog();
   flecs::world world;
-  world.set_target_fps(50);
+  world.set_target_fps(4);
   world.set_threads(4);
   world.import<flecs::monitor>();
 
@@ -18,7 +20,14 @@ int main(int argc, char const *argv[]) {
   world.import<engine::WindowBackendSfml>();
   world.import<engine::Filesystem>();
   world.import<engine::FilesystemBackendStdlib>();
+  world.import<engine::Transform>();
   world.set<engine::window::MainWindowInit>({});
+
+  world.entity().set<engine::opengl::ShaderProgramLoader>({
+    .vs_path = "resources/shader.vs",
+    .fs_path = "resources/shader.fs",
+  });
+
 
   world.app().enable_rest().run();
   return 0;
--- engine/modules/opengl.hpp
+++ engine/modules/opengl.hpp
@@ -1,6 +1,8 @@
 #pragma once
 #include <memory>
+#include <string>
 #include <flecs.h>
+#include <glad/gl.h>
 #include "opengl/GLContextLock.hpp"
 
 namespace engine {
@@ -15,6 +17,32 @@ namespace engine::opengl {
 
 struct OpenglContext {
   std::shared_ptr<GLContextLock> gl_lock; //Пока существует хоть один указатель на GLContextLock, opengl контекст не будет разрушен
+};
+
+struct GLBuffer {
+  GLuint id = GL_NONE; //dsa only //glCreateBuffers
+  std::shared_ptr<GLContextLock> lock;
+};
+
+struct GLShader {
+  GLuint id = GL_NONE;
+  std::shared_ptr<GLContextLock> lock;
+};
+
+struct VertexShader {};
+struct FragmentShader {};
+
+struct ShaderLoader {
+  std::string path;
+};
+struct ShaderProgramLoader {
+  std::string vs_path;
+  std::string fs_path;
+};
+
+struct GLShaderProgram {
+  GLuint id = GL_NONE;
+  std::shared_ptr<GLContextLock> lock;
 };
 
 } //namespace engine::opengl
