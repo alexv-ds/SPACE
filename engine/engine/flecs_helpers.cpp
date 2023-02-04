7a46fa3d3a46e404d313cb35f228e14ee7e5b98f
alex <sasichkamega@gmail.com>
2023-02-04 21:52:41 +0300

    lookup by strview

--- engine/engine/flecs_helpers.cpp
+++ engine/engine/flecs_helpers.cpp
@@ -3,6 +3,13 @@
 
 namespace engine {
 
+flecs::entity lookup_by_stringview(flecs::world& world, const std::string_view& name) {
+  static thread_local std::string name_buff;
+  name_buff.clear();
+  name_buff.append(name.begin(), name.end());
+  return world.lookup(name_buff.c_str());
+}
+
 flecs::entity entity_by_stringview(flecs::world& world, const std::string_view& name) {
   static thread_local std::string name_buff;
   name_buff.clear();
--- engine/engine/flecs_helpers.hpp
+++ engine/engine/flecs_helpers.hpp
@@ -4,6 +4,7 @@
 
 namespace engine {
 
+flecs::entity lookup_by_stringview(flecs::world&, const std::string_view&);
 flecs::entity entity_by_stringview(flecs::world&, const std::string_view&);
 
 }
