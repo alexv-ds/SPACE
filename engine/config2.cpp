cb1e5c1a750751b390cb6d41711110bfedc00c35
sasichkamega <sasichkamega@gmail.com>
2023-02-08 16:56:58 +0300

    config rework start

---
+++ engine/config2.cpp
@@ -0,0 +1,2 @@
+#include "config2.hpp"
+
---
+++ engine/config2.hpp
@@ -0,0 +1,65 @@
+#include <flecs.h>
+#include <cstdint>
+
+namespace engine::config2 {
+  namespace detail {
+    template<std::size_t Size>
+    class TemplatedString {
+    public:
+      constexpr TemplatedString(const char (&str)[Size]) {
+        for (std::size_t i = 0; i < Size; ++i) {
+          value[i] = str[i];
+        }
+        value[Size] = 0; //for uses as c-string
+      }
+      char value[Size+1];
+    };
+
+    template<class T>
+    struct TemplatedToStringFunc {
+      using FuncType = std::size_t (*)(char*, const std::size_t, const T&);
+      constexpr TemplatedToStringFunc(FuncType f): f(f) {}
+      FuncType f;
+    };
+
+    template <class Type, TemplatedString TypeName, TemplatedToStringFunc ToStringFunc>
+    struct TypedValue {
+      static inline const char* type_name() {
+        return TypeName.value;
+      }
+
+      inline TypedValue(Type&& value): value(std::move(value)) {}  
+      inline std::size_t to_string(char* buff, const std::size_t buff_size) {
+        return ToStringFunc.f(buff, buff_size, value);
+      }
+      Type value;
+    };
+
+    template<class T>
+    std::size_t testfunc(char* buff, const std::size_t size, const T& value) {return 0;}
+
+  } //namespace detail
+
+  namespace type {
+    using Int32 = detail::TypedValue<std::int32_t, "int32", detail::testfunc<std::int32_t>>;
+    using Uint32 = detail::TypedValue<std::uint32_t, "uint32", detail::testfunc<std::uint32_t>>;
+    using Bool = detail::TypedValue<bool, "bool", detail::testfunc<bool>>;
+    using Float = detail::TypedValue<float, "float", detail::testfunc<float>>;
+  } //namespace type
+
+  //void create_var(flecs::world& world, const char* name, type::Int32&& data) {}
+  //void create_var(flecs::world& world, const char* name, type::Uint32&& data) {}
+  //void create_var(flecs::world& world, const char* name, type::Bool&& data) {}
+  //void create_var(flecs::world& world, const char* name, type::Float&& data) {}
+
+  template <class T>
+  void create_var(flecs::world& world, const char* name, T&& data) {}
+
+
+  void keks() {
+    flecs::world world;
+    create_var<type::Int32>(world, "123", 123);
+  }
+
+
+} //namespace engine::config2
