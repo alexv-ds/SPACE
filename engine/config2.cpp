181329e328bfd1b97880b80f82b4d7da63354438
sasichkamega <sasichkamega@gmail.com>
2023-02-10 16:48:58 +0300

    work on config2

--- engine/config2.cpp
+++ engine/config2.cpp
@@ -1,11 +1,30 @@
+#include <spdlog/spdlog.h>
 #include "config2.hpp"
 
 namespace engine::config2 {
 
-  template <class T>
-  void create_var(flecs::world& world, const char* name, T&& data) {}
+  namespace detail {
 
-  template void create_var<type::Int32>(flecs::world& world, const char* name, type::Int32&& data);
+
+  };
+
+  template <class T>
+  flecs::entity create_var(flecs::world& world, const char* name, T&& data) {
+    flecs::entity entity = world.entity(name);
+    return entity;
+  }
+
+  template flecs::entity create_var<type::Int32>(flecs::world& world, const char* name, type::Int32&& data);
 
 
 } //namespace engine::config2
+
+engine::Config2::Config2(flecs::world& world) {
+  using namespace config2;
+  world.module<Config2>("config2");
+
+  world.component<type::Int32>("Int32")
+    .member<decltype(type::Int32::value)>("value");
+
+
+};
--- engine/config2.hpp
+++ engine/config2.hpp
@@ -1,63 +1,33 @@
 #include <flecs.h>
 #include <cstdint>
-#include <string>
+#include <functional>
+
+namespace engine {
+  struct Config2 {
+    Config2(flecs::world&);
+  };
+};
 
 namespace engine::config2 {
-  namespace detail {
-    template<std::size_t Size>
-    class TemplatedString {
-    public:
-      constexpr TemplatedString(const char (&str)[Size]) {
-        for (std::size_t i = 0; i < Size; ++i) {
-          value[i] = str[i];
-        }
-        value[Size] = 0; //for uses as c-string
-      }
-      char value[Size+1];
-    };
-
-    template<class T>
-    struct TemplatedToStringFunc {
-      using FuncType = std::size_t (*)(char*, const std::size_t, const T&);
-      constexpr TemplatedToStringFunc(FuncType f): f(f) {}
-      FuncType f;
-    };
-
-    template <class Type, TemplatedString TypeName, TemplatedToStringFunc ToStringFunc>
-    struct TypedValue {
-      static inline const char* type_name() {
-        return TypeName.value;
-      }
-
-      inline TypedValue(Type&& value): value(std::move(value)) {}  
-      inline std::size_t to_string(char* buff, const std::size_t buff_size) {
-        return ToStringFunc.f(buff, buff_size, value);
-      }
-      //inline 
-      Type value;
-    };
-
-    template<class T>
-    std::size_t testfunc(char* buff, const std::size_t size, const T& value) {return 0;}
-
-  } //namespace detail
+  struct ConfigComponent {};
+  struct ValidatedData {};
+  struct UnValidatedData {};
 
   namespace type {
-    using Int32 = detail::TypedValue<std::int32_t, "int32", detail::testfunc<std::int32_t>>;
-    using Uint32 = detail::TypedValue<std::uint32_t, "uint32", detail::testfunc<std::uint32_t>>;
-    using Bool = detail::TypedValue<bool, "bool", detail::testfunc<bool>>;
-    using Float = detail::TypedValue<float, "float", detail::testfunc<float>>;
-    //using String = detail::TypedValue<std::string, "string", detail::testfunc<std::string>>;
+    struct Int32 {
+      inline Int32(std::int32_t value) noexcept: value(value) {}
+      std::int32_t value;
+    };
+
+    struct Uint32 {
+      inline Uint32(std::uint32_t value) noexcept: value(value) {}
+      std::uint32_t value;
+    };
   } //namespace type
 
   template <class T>
-  void create_var(flecs::world& world, const char* name, T&& data);
+  flecs::entity create_var(flecs::world& world, const char* name, T&& data);
 
-
-  void keks() {
-    flecs::world world;
-    create_var<std::uint64_t>(world, "123", 123);
-  }
 
 
 } //namespace engine::config2
